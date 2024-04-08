93b87119ecb677af792b286082aeb4e936746ebf
Cindy <63568702+CindyU-beep@users.noreply.github.com>
2024-04-08 12:13:30 +1000

    added in user register/login paths in backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 
-//Routes
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//Home
 app.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to the Helpdesk" })
 })
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is started running on port ${PORT}`))
