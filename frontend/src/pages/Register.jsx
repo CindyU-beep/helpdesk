d289c25e8ae88b03df3716bfb1d5fd93567ab550
Cindy <63568702+CindyU-beep@users.noreply.github.com>
2024-05-04 11:31:45 +1000

    auth register/logout

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,9 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {FaUser} from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import {useSelector, useDispatch} from 'react-redux';
-import {register} from '../features/auth/authSlice';
+import {register, reset} from '../features/auth/authSlice';
+import {useNavigate} from 'react-router-dom';
 
 function Register() {
 
@@ -15,11 +16,12 @@ function Register() {
 
     const {name, email, password, confirmPassword} = formData
     
-    //initialise dispatch
+    //initialise actions
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     //get state
-    const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
+    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
 
     const onChange =(e) => {
         setFormData((prevState) => ({
@@ -28,22 +30,31 @@ function Register() {
         }))
     }
 
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        } 
+        //redirect when logged in
+        if(isSuccess || user) {
+            navigate('/home')            
+        }
+        dispatch(reset)
+
+    }, [isError, isSuccess, user, message, navigate, dispatch])
+
     const onSubmit = (e) => {
        e.preventDefault()
        
        if (password !== confirmPassword) {
-        toast.error("Passwords do not match")
+            toast.error("Passwords do not match")
        } else {
-        const userData = {
-            name,
-            email,
-            password
-        }
-
-        dispatch(register(userData))
+            const userData = {
+                name,
+                email,
+                password
+            }
+            dispatch(register(userData))
        }
-    
-
     }
 
     return(
