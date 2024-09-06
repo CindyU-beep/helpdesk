968324ccbdd425dc8675321693d77330dc8d5d82
Cindy's Desktop <cindy.um73@gmail.com>
2024-09-06 08:46:09 +0200

    prep for deploying in heroku

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const colors = require('colors');
+const path = require('path')
+
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
-
 const PORT = process.env.PORT || 8000
 
 //Connect to MongoDB Database
@@ -14,14 +15,22 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//Home
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "Welcome to the Helpdesk" })
-})
-
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 app.use(errorHandler)
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    //setup build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    //Home
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: "Welcome to the Helpdesk" })
+    })
+}
 
 app.listen(PORT, () => console.log(`Server is started running on port ${PORT}`))
